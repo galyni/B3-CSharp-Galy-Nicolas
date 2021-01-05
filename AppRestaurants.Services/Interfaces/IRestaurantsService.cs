0d12d985fe515769ba2618f0377cccac0161457c
Nicolas Galy <ngaly@hotmail.fr>
2021-01-05 10:17:27 +0100

    services tweaks

---
+++ AppRestaurants.Services/Interfaces/IRestaurantsService.cs
@@ -0,0 +1,13 @@
+﻿using AppRestaurants.Data.Models;
+using System.Collections.Generic;
+
+namespace AppRestaurants.Services {
+    public interface IRestaurantsService {
+        void CreateRestaurant(Restaurant restaurant);
+        void DeleteRestaurant(int id);
+        Restaurant GetRestaurantById(int id);
+        List<Restaurant> GetRestaurantsList();
+        List<Restaurant> GetTopFiveWithGrades();
+        void UpdateRestaurant(Restaurant restaurant);
+    }
+}
--- AppRestaurants.Services/RestaurantsService.cs
+++ AppRestaurants.Services/RestaurantsService.cs
@@ -1,12 +1,13 @@
 ﻿using AppRestaurants.Data.Db;
 using AppRestaurants.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace AppRestaurants.Services {
-    class RestaurantsService {
+    public class RestaurantsService : IRestaurantsService {
         private RestaurantsContext _ctx;
         public RestaurantsService(RestaurantsContext ctx) {
             _ctx = ctx;
@@ -15,12 +16,16 @@ namespace AppRestaurants.Services {
         public virtual List<Restaurant> GetRestaurantsList() {
             return _ctx.Restaurants.ToList();
         }
-        
+
+        public virtual List<Restaurant> GetTopFiveWithGrades() {
+            return _ctx.Restaurants.Include(r => r.LastGrade).OrderByDescending(r => r.LastGrade.Note).Take(5).ToList();
+        }
+
         public virtual Restaurant GetRestaurantById(int id) {
             return _ctx.Restaurants.Find(id);
 
         }
-        
+
         public virtual void CreateRestaurant(Restaurant restaurant) {
             _ctx.Restaurants.Add(restaurant);
             _ctx.SaveChanges();
